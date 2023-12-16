564208a226e0927602ead034b58b70264542a62e
cristian <scarlat.cristian.gabriel@gmail.com>
2023-12-16 23:16:17 +0200

    basic layout

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 from flask import Flask
+import random
+
+correct_route=random.randint(1, 10)
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route('/<int:num>')
+def check_route(num):
+    if num == correct_route:
+        return '<h1>Higher</h1>'
+    elif num > correct_route:
+        return '<h1>Lower</h1>'
+    else:
+        return '<h1>Correct</h1>'
 
-@app.route("/bye")
-def say_bye():
-    return "<p>Bye peeps</p>"
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
